675aef5530941b9ebe4534775a153296db01911a
GrzaNNa <117536463+AGrzaNNa@users.noreply.github.com>
2024-02-27 23:08:10 +0100

    Update main.py
    add comments

--- Project #1/main.py
+++ Project #1/main.py
@@ -1,8 +1,11 @@
 # Import necessary functions
-from caesar_cipher import *
-from vigenere_cipher import *
+from caesar_cipher import *  # Import functions related to Caesar Cipher
+from vigenere_cipher import *  # Import functions related to Vigenère Cipher
 
 def main_menu():
+    """
+    Display the main menu and handle user choices.
+    """
     while True:
         print("\nWelcome to Scrypt Panel!\n")
         print("1. Caesar Cipher")
@@ -20,8 +23,10 @@ def main_menu():
         else:
             print("Invalid choice. Please select a valid option.")
 
-
 def caesar_cipher_menu():
+    """
+    Display the Caesar Cipher menu and handle user choices.
+    """
     while True:
         print("\n")
         print("1. Crypt code")
@@ -39,7 +44,10 @@ def caesar_cipher_menu():
             print("Invalid choice. Please select a valid option.")
 
 def encrypt_text():
-    input_text = input("\n""Write word: ")
+    """
+    Encrypt text using the Caesar Cipher.
+    """
+    input_text = input("\nWrite word: ")
     if validate_word(input_text):
         shift = int(input("Write shift: "))
         encrypted_text = caesar_cipher(input_text, shift)
@@ -47,24 +55,28 @@ def encrypt_text():
     else:
         print(f"The word '{input_text}' is not in the English dictionary.")
 
-
 def decrypt_text():
+    """
+    Decrypt text using the Caesar Cipher.
+    """
     print("1. Decrypt code manually")
     print("2. Decrypt code automatically")
     choice = input("Select option: ")
 
     if choice == '1':
-        input_text = input("\n""Write word: ")
+        input_text = input("\nWrite word: ")
         shift = int(input("Write shift: "))
         print("Decrypted text:", caesar_cipher_decrypt(input_text, shift))
     elif choice == '2':
-        input_text = input("\n""Write word: ")
+        input_text = input("\nWrite word: ")
         print("Decrypted text:", caesar_cipher_decrypt_auto(input_text))
     else:
         print("Invalid choice")
 
-
 def vigenere_cipher_menu():
+    """
+    Display the Vigenère Cipher menu and handle user choices.
+    """
     while True:
         print("\n")
         print("1. Crypt code")
@@ -81,30 +93,33 @@ def vigenere_cipher_menu():
         else:
             print("Invalid choice. Please select a valid option.")
 
-
 def encrypt_text_vigenere():
-    plaintext = input("\n""Write word: ")
+    """
+    Encrypt text using the Vigenère Cipher.
+    """
+    plaintext = input("\nWrite word: ")
     if validate_word(plaintext):
         key = input("Write key: ")
         print("Encrypted text:", vigenere_encrypt(plaintext, key))
     else:
         print(f"The word '{plaintext}' is not in the English dictionary.")
 
-
 def decrypt_text_vigenere():
+    """
+    Decrypt text using the Vigenère Cipher.
+    """
     print("1. Decrypt code manually")
     print("2. Decrypt code automatically")
     choice = input("Select option: ")
     if choice == '1':
-        plaintext = input("\n""Write word: ")
+        plaintext = input("\nWrite word: ")
         key = input("Write key: ")
         print("Decrypted text:", vigenere_decrypt(plaintext, key))
     elif choice == '2':
-        ciphertext = input("\n""Write word: ")
+        ciphertext = input("\nWrite word: ")
         print("Decrypted text:", break_vigenere(ciphertext, 3))
     else:
         print("Invalid choice")
-
 
 if __name__ == "__main__":
     main_menu()
